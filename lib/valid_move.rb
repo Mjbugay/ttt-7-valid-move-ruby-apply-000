4b58249a8f8056585f09372b0ecd7c3bca56db49
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-08-27 18:12:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,17 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
    def position_taken?(board, index)
-    if board[index]  == " " || board[index] == "" || board[index] == nil
-      return true
-    else 
-       return false
-   end 
-  if index.between?(0,8) && !position_taken?(board,index) == true
+    if board[index] == " "
+    return false
+   elsif board[index] == ""
+    return false
+   elsif board[index] == nil
+    return false
+   else board[index] == "X" || "O"
+    return true
+  end  
+end    
+  if index.between?(0,8) && !position_taken?(board,index)
     return true
   else 
     return false
